e54945970aab72c250a6f622f4d7459b49605a73
Maxmarra <5919669@gmail.com>
2022-05-23 10:55:36 +0300

    starter

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -7,14 +7,11 @@ import androidx.lifecycle.ViewModel
 class OverviewViewModel : ViewModel() {
 
     private val _status = MutableLiveData<String>()
-
-
     val status: LiveData<String> = _status
 
     init {
         getMarsPhotos()
     }
-
 
     private fun getMarsPhotos() {
         _status.value = "Set the Mars API status response here!"
