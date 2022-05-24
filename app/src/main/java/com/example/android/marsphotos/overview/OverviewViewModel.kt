0adbdf0ed8a94e8d0f8297233c8c8ccd8a7eb7c3
Maxmarra <5919669@gmail.com>
2022-05-24 10:02:21 +0300

    04_Display_an_internet_image

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -5,12 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
+
+//Извлечение и работа с ОДНИМ ОБЪЕКТОМ
+//Вывод одной фотки
 
 class OverviewViewModel : ViewModel() {
 
     private val _status = MutableLiveData<String>()
     val status: LiveData<String> = _status
+
+    private val _photos = MutableLiveData<MarsPhoto>()
+    val photos: LiveData<MarsPhoto> = _photos
 
     init {
         getMarsPhotos()
@@ -19,8 +26,8 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsPhotos() {
         viewModelScope.launch {
             try {
-                val listResult = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+                _photos.value = MarsApi.retrofitService.getPhotos()[0]
+                _status.value = "First image URL: ${_photos.value!!.imgSrcUrl}"
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
             }
