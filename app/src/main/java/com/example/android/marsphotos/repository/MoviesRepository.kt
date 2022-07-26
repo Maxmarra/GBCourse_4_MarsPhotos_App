20570a0991a8b06595975bd969c3d7da2908e7a3
Maxmarra <5919669@gmail.com>
2022-07-26 13:42:23 +0300

    movie db with repo part 2

--- app/src/main/java/com/example/android/marsphotos/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/android/marsphotos/repository/MoviesRepository.kt
@@ -3,6 +3,7 @@ package com.example.android.marsphotos.repository
 import com.example.android.marsphotos.database.MovieDatabase
 import com.example.android.marsphotos.network.API_KEY
 import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -11,6 +12,8 @@ class MoviesRepository(private val database: MovieDatabase) {
     suspend fun refreshMovies() {
         withContext(Dispatchers.IO) {
             val movieList = MarsApi.retrofitService.getPopularMovies(apiKey = API_KEY)
+
+            database.movieDao.insertAllMovies(movieList.asDatabaseModel())
         }
     }
 }
